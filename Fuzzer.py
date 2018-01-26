5805d5feb547f89e540abaa641aa64f3420bd323
Gandosha <Gandosha@users.noreply.github.com>
2018-01-26 08:17:00 -0500

    Update Fuzzer.py

--- Fuzzer.py
+++ Fuzzer.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python
 import socket
 
-buffer = ["A"]
-counter = 50
-
-while len(buffer) <= 1000:
-    buffer.append("A" * counter)
-    counter = counter + 50
-
-for buffstring in buffer:
-    print "Fuzzing:" + str(len(buffstring))
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect( ("<IP_ADDRESS_TO_FUZZ>", <PORT_TO_FUZZ>) )
-    sock.send(buffstring)
-    sock.close()
+for i in range(30):
+ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+ s.connect(('<TARGET_IP_ADDRESS>',<TARGET_PORT>))
+ payload = int(i)*("A"*100)
+ s.send(payload+"\r\n")
+ print "[Info] Fuzzing with " + str(len(str(payload))) + " A's\r\n"
+ s.close()
